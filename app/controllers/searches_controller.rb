e60e4904ad1717b2255c50db7ceeaf734b2a124b
EC2 Default User <ec2-user@ip-172-31-26-40.ap-northeast-1.compute.internal>
2024-05-28 06:19:50 +0000

    fixed layout

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user_or_admin!
 
   def search
     @range = params[:range]
@@ -13,6 +13,14 @@ class SearchesController < ApplicationController
       render "searches/search_result"
     else
       redirect_to request.referer
+    end
+  end
+  private
+
+  def authenticate_user_or_admin!
+    unless user_signed_in? || admin_signed_in?
+      # ユーザーも管理者もサインインしていない場合、適切なリダイレクトやエラーメッセージを設定します
+      redirect_to new_user_session_path, alert: "You need to sign in as a user or admin."
     end
   end
 end
