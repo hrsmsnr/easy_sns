131ebef9e3f1ed83545a2004d6c9352fcf166e02
EC2 Default User <hirose0201masanori@gmail.com>
2024-06-29 09:58:50 +0000

    fixed destroy_comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,4 +1,6 @@
 class Public::CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_matching_login_user_or_admin, only: [:destroy]
 
   def create
     @post = Post.find(params[:post_id])
@@ -17,10 +19,8 @@ class Public::CommentsController < ApplicationController
   end
 
   def destroy
-    if comment.user == current_user || admin_signed_in?
-      Comment.find(params[:id]).destroy
-      flash[:success] = 'コメントの削除が完了しました'
-    end
+    Comment.find(params[:id]).destroy
+    flash[:success] = 'コメントの削除が完了しました'
     redirect_to post_path(params[:post_id])
   end
 
@@ -28,6 +28,13 @@ class Public::CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def is_matching_login_user_or_admin
+    @comment = Comment.find(params[:id])
+    unless @comment.user == current_user || admin_signed_in?
+      redirect_to post_path(params[:post_id])
+    end
   end
 
 end
